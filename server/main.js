e6014b4f14bd6cd3b790a71d76df6b145fef8ce6
Matthew Francis-Landau <matthew@matthewfl.com>
2012-10-13 08:24:43 -0700

    got system to take data from flicker and add it into the results

--- server/main.js
+++ server/main.js
@@ -2,14 +2,17 @@ var express = require('express');
 var http = require('http');
 var https = require('https');
 var fs = require('fs');
+var async = require('./async').async;
 
-var instaToken = "235597193.173372b.50bdb5394d7b4063b8147f10b928532d";
+var instaToken = "235597193.173372b.50bdb5394d7b4063b8147f10b928532d"; // first token
+//var instaToken = "235597193.fc50470.fb1fab07e49a4cdcb9f45344e834d51a"; // second token
 
-function getPicsBefore (lat, lon, dist_km, before, callback) {
+function getPicsBefore (lat, lon, dist_km, before,  callback) {
+    if(dist_km > 5) dist_km = 5;
     var url = "https://api.instagram.com/v1/media/search?lat="+lat+"&lng="+lon+"&distance="+(dist_km*1000);
-    if (before) url += "&max_timestamp=" + before;
-    url += "&access_token=" + instaToken; // +"&callback=?";
-    //console.log(url);
+    if(before) url += "&max_timestamp="+before;
+    url += "&access_token="+instaToken; // +"&callback=?";
+    console.log(url);
 
     // the 0 image is the newest image
     /*$.ajax({
@@ -21,9 +24,21 @@ function getPicsBefore (lat, lon, dist_km, before, callback) {
       });*/
 
     https.get(url, function (ev) {
-        var buffer = "";
-        ev.on('data', function (buf) {
-            buffer += buf.toString();
+	var buffer = "";
+
+	console.log("instagram headers: ", ev.headers);
+
+	ev.on('data', function (buf) {
+	    buffer += buf.toString();
+	});
+	ev.on('end', function () {
+	    var j = [];
+	    try {
+		j = JSON.parse(buffer);
+	    } catch(e) {
+		console.log("error ", buffer)
+	    }
+	    callback(j);
 	});
         ev.on('end', function () {
             var j = [];
@@ -62,6 +77,121 @@ function getPics (lat, lon, dist_km, count, callback, timestamp) {
         }
 	getMore();
     })(ret);
+}
+
+
+// flicker
+
+// { "id": "8081614731", "owner": "23119895@N00", "secret": "8ac217ef5f", "server": "8475", "farm": 9, "title": "Pumpkins", "ispublic": 1, "isfriend": 0, "isfamily": 0, "datetaken": "2012-10-07 16:07:06", "datetakengranularity": 0, "latitude": 37.475167, "longitude": -122.418167, "accuracy": 16, "context": 0, "place_id": "N8DyJRJTVrsXl6Uo", "woeid": "2416227", "geo_is_family": 0, "geo_is_friend": 0, "geo_is_contact": 0, "geo_is_public": 1 , views: 26 },
+// flicker url: http://farm{farm}.static.flickr.com/{server}/{id}_{secret}_z.jpg'
+
+/* instagram:
+   {
+   "data": [{
+   "distance": 41.741369194629698,
+   "type": "image",
+   "filter": "Earlybird",
+   "tags": [],
+   "comments": { ... },
+   "caption": null,
+   "likes": { ... },
+   "link": "http://instagr.am/p/BQEEq/",
+   "user": {
+   "username": "mahaface",
+   "profile_picture": "http://distillery.s3.amazonaws.com/profiles/profile_1329896_75sq_1294131373.jpg",
+   "id": "1329896"
+   },
+   "created_time": "1296251679",
+   "images": {
+   "low_resolution": {
+   "url": "http://distillery.s3.amazonaws.com/media/2011/01/28/0cc4f24f25654b1c8d655835c58b850a_6.jpg",
+   "width": 306,
+   "height": 306
+   },
+   "thumbnail": {
+   "url": "http://distillery.s3.amazonaws.com/media/2011/01/28/0cc4f24f25654b1c8d655835c58b850a_5.jpg",
+   "width": 150,
+   "height": 150
+   },
+   "standard_resolution": {
+   "url": "http://distillery.s3.amazonaws.com/media/2011/01/28/0cc4f24f25654b1c8d655835c58b850a_7.jpg",
+   "width": 612,
+   "height": 612
+   }
+   },
+   "id": "20988202",
+   "location": null
+   },
+*/
+
+
+function flicker2insta (pic) {
+    var url = ("http://farm{farm}.static.flickr.com/{server}/{id}_{secret}_z.jpg")
+	.replace(/{farm}/g, pic.farm)
+	.replace(/{server}/g, pic.server)
+	.replace(/{id}/g, pic.id)
+	.replace(/{secret}/g, pic.secret);
+    return {
+	"created_time": new Date(pic.datetaken)*1/1000 + "",
+	"id": "flicker_"+pic.id,
+	"likes": {
+	    "count": Math.floor(pic.views/10),
+	},
+	"location": {
+	    "latitude": pic.latitude,
+	    "longitude": pic.longitude
+	},
+	"images": {
+	    "low_resolution": {
+		"url": url,
+		"width": 306,
+		"height": 306
+	    },
+	    "thumbnail": {
+		"url": url,
+		"width": 150,
+		"height": 150
+	    },
+	    "standard_resolution": {
+		"url": url,
+		"width": 612,
+		"height": 612
+	    }
+	}
+    };
+}
+
+function flickerGet (lat, lon, dist_km, count, callback, timestamp) {
+    var ret = [];
+    if(dist_km > 20) dist_km = 20;
+    var url = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=45165da8fef2794bea1b10800c19c67e&lat="+lat+"&lon="+lon+"&radius="+dist_km+
+	"&extras=date_taken%2Cgeo%2Cviews&per_page="+count+"&format=json&nojsoncallback=1"
+
+    console.log(url);
+    http.get(url , function (ev) {
+	var buffer = "";
+	ev.on('data', function (dat) {
+	    buffer += dat.toString();
+	});
+	ev.on('end',function () {
+	    var j = [];
+	    try {
+		console.log(buffer);
+		var result = JSON.parse(buffer);
+		for(var i=0; i < result.photos.photo.length; i++) {
+		    j.push(flicker2insta(result.photos.photo[i]));
+		}
+
+	    }catch(e) {
+		console.log('flicker error', e);
+	    }
+
+	    callback(j);
+	});
+    }).on('error', function (err) {
+	console.log('Flicker error ', err);
+	callback([]);
+    });
 }
 
 // hold a catch of data that has been already returned
@@ -166,6 +296,12 @@ app.get('/_test', function (req, res) {
     });
 });
 
+app.get('/ftest', function (req, res) {
+    flickerGet(37.483, -122.15, 10, 10, function (c) {
+	res.end(JSON.stringify(c));
+    });
+});
+
 app.get('/_cache', function (req, res) {
     res.end('making cache');
     getPics(37.483, -122.15, 10, 10, function (c) {
@@ -189,13 +325,23 @@ app.get('/data', function(req, res) {
     var ret = search_cache(lat, lon, dist);
     var count = 100 - ret.length;
     if(count < 10) count = 10;
-    getPics(lat, lon, count, dist, function (c) {
-	ret = ret.concat(c); // should clean the ret array
-	cache = cache.concat(c);
-	update_cache();
-	save_cache();
-	res.end(JSON.stringify(ret));
-    });
+    async([
+	[
+	    function () {
+		getPics(lat, lon, dist, count, this);
+	    },
+	    function () {
+		flickerGet(lat, lon, dist, count, this);
+	    }
+	],
+	function () {
+	    ret = ret.concat(this[0]).concat(this[1]);
+	    cache = cache.concat(this[0]).concat(this[1]);
+	    update_cache();
+	    save_cache();
+	    res.end(JSON.stringify(ret));
+	}
+    ]);
 });
 
 
